aa3215bfa8f886f7c9d3421e928e652ed52891e6
Hung-I Wang <whygowe@gmail.com>
2020-09-14 14:14:06 +0800

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # entrypoint for Docker
 set -e
 if [ -z "$SESSION_NAME" ]; then
-    mkdir /app/session
+    mkdir -p /app/session
     export SESSION_NAME="/app/session/anon.session"
 fi
 exec python -m telegram_user_tracker
